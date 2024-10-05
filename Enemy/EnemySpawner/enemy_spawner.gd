a0c4809f2e3fafeec90559c003cd1ad75e5eca25
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2024-10-05 14:12:16 -0600

    Add enemy spawner
    Should work??

--- Enemy/EnemySpawner/enemy_spawner.gd
+++ Enemy/EnemySpawner/enemy_spawner.gd
@@ -2,8 +2,41 @@ extends Node2D
 
 
 """
-Holds the enemies and spawns them in.
+Works its way through the list, spawning enemies 
 """
+@export var enemy_spawner_instructions:Array[SingleEnemySpawnInstruction] = []#:EnemySpawnInstructions
+
+var current_time_to_spawn: float:
+	set(val):
+		current_time_to_spawn = val
+		if  current_time_to_spawn <= 0:
+			_spawn_next()
+		pass 
+var instruction_index: int = 0 
+var current_instructions:SingleEnemySpawnInstruction
 
 
-var basic_alien:PackedScene = preload("res://Enemy/alien.tscn")
+
+
+func _ready():
+	
+	pass
+
+func _process(delta: float) -> void:
+	
+	pass
+
+func _spawn_next():
+	instruction_index += 1
+	current_instructions = enemy_spawner_instructions[instruction_index]
+	
+	var new_enemy = current_instructions.enemy_scene.instantiate()
+	add_child(new_enemy)
+	new_enemy.position = current_instructions.position
+	if current_instructions.state != -1:
+		#TODO fix this breaking on no state
+		new_enemy.state = current_instructions.state
+	
+	
+	
+	pass
