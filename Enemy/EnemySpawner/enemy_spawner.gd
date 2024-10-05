c8ed6b80738b10df808644d1ffdfc382344fe388
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2024-10-05 14:25:54 -0600

    Fix enemy spawner
    Can edit the export to spawn enemies
    Scene selection narrowing not done yet.

--- Enemy/EnemySpawner/enemy_spawner.gd
+++ Enemy/EnemySpawner/enemy_spawner.gd
@@ -19,24 +19,30 @@ var current_instructions:SingleEnemySpawnInstruction
 
 
 func _ready():
-	
+	current_time_to_spawn = enemy_spawner_instructions[0].time_offset
 	pass
 
 func _process(delta: float) -> void:
-	
+	current_time_to_spawn -= delta
 	pass
 
 func _spawn_next():
-	instruction_index += 1
-	current_instructions = enemy_spawner_instructions[instruction_index]
-	
-	var new_enemy = current_instructions.enemy_scene.instantiate()
-	add_child(new_enemy)
-	new_enemy.position = current_instructions.position
-	if current_instructions.state != -1:
-		#TODO fix this breaking on no state
-		new_enemy.state = current_instructions.state
-	
-	
-	
-	pass
+	if instruction_index < enemy_spawner_instructions.size():
+			
+		current_instructions = enemy_spawner_instructions[instruction_index]
+		
+		var new_enemy = current_instructions.enemy_scene.instantiate()
+		add_child(new_enemy)
+		new_enemy.position = current_instructions.position
+		if current_instructions.state != -1:
+			#TODO fix this breaking on no state
+			new_enemy.state = current_instructions.state
+		
+		##this is changed at the end so that it keeps the current time rather than being time for the next spawn.
+		instruction_index += 1
+		#double check bad :(
+		if instruction_index < enemy_spawner_instructions.size():
+			current_time_to_spawn = enemy_spawner_instructions[instruction_index].time_offset
+		
+		
+		pass
