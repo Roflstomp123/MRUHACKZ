7d48dcfe1090458dfb07a590a7bacc8c3b1a1fc5
Roflstomp123 <buzac03@gmail.com>
2024-10-05 15:43:29 -0600

    Revert "Alien attack"
    This reverts commit c6940b2132efe5e0b69f73d875a1bced83350f67.

--- Enemy/alien.gd
+++ Enemy/alien.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-var Alienattack:PackedScene = preload("res://Enemy/alien.tscn")
+
 
 const SPEED = 50
 
@@ -7,17 +7,9 @@ const SPEED = 50
 func _physics_process(delta: float) -> void:
 	velocity.y = SPEED
 	move_and_slide()
-
-
-
+	
+	
+	
 func kill():
 	queue_free()
-	
-	
-func shoot():
-	pass
-	
-	
-	
-	
 	
