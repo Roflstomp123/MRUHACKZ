41a6f8f0c003ba3200525bf4df2f65ac598ce639
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2024-10-06 08:17:37 -0600

    Fix level end

--- Enemy/bullet.gd
+++ Enemy/bullet.gd
@@ -6,3 +6,5 @@ func _process(delta):
 	var direction = -Vector2.RIGHT.rotated(rotation)
 	position += direction * speed * delta
 	pass
+	if abs(position.x)  > 1500 or abs(position.y) > 1300:
+		queue_free()
